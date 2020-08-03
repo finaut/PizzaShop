d9ac07e14f4a2a3dbe4c0c2f07bfcaef7d67d456
Danila <finaut477@gmail.com>
2020-08-03 10:31:05 +0300

    add migrate

--- db/migrate/20200803063307_create_products.rb
+++ db/migrate/20200803063307_create_products.rb
@@ -1,5 +1,17 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
-    cre
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      t.decimal :price
+      t.decimal :size
+      t.boolean :is_spicy # буль тип острая да или нет
+      t.boolean :is_veg # спрашиваем веган
+      t.boolean :is_best_offer # админ сможет держать вверху лучшие предложение
+      t.string :path_to_image # путь расположение фотографии
+
+
+      t.timestamps
+    end
   end
 end
